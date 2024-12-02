75b8f0df9eac0290267597c132086d316bdc7bc1
Theo Crandall <theo.crandall@computronix.com>
2024-12-02 15:15:40 -0700

    2020 Day 8

---
+++ 2020/python/day8.py
@@ -0,0 +1,65 @@
+from solution import Solution
+
+
+class Day8(Solution):
+    @classmethod
+    def _Part1(cls) -> int:
+        acc = 0
+        instructions = {i: line.split(" ") for i, line in enumerate(cls.inputLines)}
+        current = 0
+        visited = set()
+        while current not in visited:
+            visited.add(current)
+            instruction, arg = instructions[current]
+            if instruction == "nop":
+                current += 1
+                continue
+            elif instruction == "acc":
+                acc += int(arg)
+            elif instruction == "jmp":
+                current += int(arg) - 1
+
+            current += 1
+
+        return acc
+
+    @classmethod
+    def _Part2(cls) -> int:
+        instructions = {i: line.split(" ") for i, line in enumerate(cls.inputLines)}
+        fixesToTry = set()
+        triedFixes = set()
+        lastFixTried = -1
+        while True:
+            visited = set()
+            acc = current = 0
+            while current not in visited:
+                if current == len(instructions):
+                    return acc
+
+                visited.add(current)
+                instruction, arg = instructions[current]
+                if instruction == "nop":
+                    if current not in fixesToTry and current not in triedFixes:
+                        fixesToTry.add(current)
+                elif instruction == "acc":
+                    acc += int(arg)
+                elif instruction == "jmp":
+                    if current not in fixesToTry and current not in triedFixes:
+                        fixesToTry.add(current)
+                    current += int(arg) - 1
+
+                current += 1
+            else:
+                if lastFixTried >= 0:
+                    instructions[lastFixTried][0] = (
+                        "jmp" if instructions[lastFixTried][0] == "nop" else "nop"
+                    )
+
+                lastFixTried = fixesToTry.pop()
+                triedFixes.add(lastFixTried)
+                instructions[lastFixTried][0] = (
+                    "jmp" if instructions[lastFixTried][0] == "nop" else "nop"
+                )
+
+
+Day8.Run("day8.txt")
