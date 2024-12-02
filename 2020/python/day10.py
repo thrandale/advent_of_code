c6a1d617f9f26c88ccd434fe7d164de1f068234e
Theo Crandall <theo.crandall@computronix.com>
2024-12-02 16:23:38 -0700

    2020 Day 10

---
+++ 2020/python/day10.py
@@ -0,0 +1,39 @@
+from functools import cache
+from solution import Solution
+
+
+class Day10(Solution):
+    parseLinesAsInt = True
+
+    @classmethod
+    @cache
+    def __CountArrangements(cls, i: int = 0) -> int:
+        if i == len(cls.inputLines) - 1:
+            return 1
+
+        count = 0
+        for j in range(i + 1, len(cls.inputLines)):
+            if cls.inputLines[j] - cls.inputLines[i] <= 3:
+                count += cls.__CountArrangements(j)
+            else:
+                break
+
+        return count
+
+    @classmethod
+    def _Part1(cls) -> int:
+        cls.inputLines.sort()
+        diffs = [0, 0, 0]
+        for i in range(len(cls.inputLines)):
+            diffs[cls.inputLines[i] - (cls.inputLines[i - 1] if i > 0 else 0) - 1] += 1
+
+        return diffs[0] * (diffs[2] + 1)
+
+    @classmethod
+    def _Part2(cls) -> int:
+        cls.inputLines.sort()
+        cls.inputLines = [0] + cls.inputLines
+        return cls.__CountArrangements()
+
+
+Day10.Run("day10.txt")
