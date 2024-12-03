b04a0e13f31e273a441e3ec937ddd1c7ebb7d9f1
Theo Crandall <theo.crandall@computronix.com>
2024-12-03 16:23:58 -0700

    2020 Day 12

---
+++ 2020/python/day12.py
@@ -0,0 +1,80 @@
+from enum import IntEnum
+from solution import Solution
+
+
+class Direction(IntEnum):
+    N = 0
+    E = 1
+    S = 2
+    W = 3
+
+
+class Day12(Solution):
+    @classmethod
+    def _Part1(cls) -> int:
+        pos = [0, 0]
+        facing = Direction.E
+
+        for line in cls.inputLines:
+            action, value = line[0], int(line[1:])
+
+            match action:
+                case "F":
+                    direction = facing
+                case "R":
+                    facing = Direction((facing.value + value // 90) % 4)
+                    continue
+                case "L":
+                    facing = Direction((facing.value - value // 90) % 4)
+                    continue
+                case _:
+                    direction = Direction[action]
+
+            match direction:
+                case Direction.N:
+                    pos[1] += value
+                case Direction.E:
+                    pos[0] += value
+                case Direction.S:
+                    pos[1] -= value
+                case Direction.W:
+                    pos[0] -= value
+
+        return abs(pos[0]) + abs(pos[1])
+
+    @classmethod
+    def _Part2(cls) -> int:
+        pos = [0, 0]
+        waypoint = [10, 1]
+
+        for line in cls.inputLines:
+            action, value = line[0], int(line[1:])
+
+            match action:
+                case "F":
+                    pos[0] += waypoint[0] * value
+                    pos[1] += waypoint[1] * value
+                    continue
+                case "R":
+                    for _ in range(value // 90):
+                        waypoint[0], waypoint[1] = waypoint[1], -waypoint[0]
+                    continue
+                case "L":
+                    for _ in range(value // 90):
+                        waypoint[0], waypoint[1] = -waypoint[1], waypoint[0]
+                    continue
+                case _:
+                    match Direction[action]:
+                        case Direction.N:
+                            waypoint[1] += value
+                        case Direction.E:
+                            waypoint[0] += value
+                        case Direction.S:
+                            waypoint[1] -= value
+                        case Direction.W:
+                            waypoint[0] -= value
+
+        return abs(pos[0]) + abs(pos[1])
+
+
+Day12.Run("day12.txt")
