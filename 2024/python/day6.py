e439c3d4e3af5703094723c578bccac76d08d784
Theo Crandall <thrandale@gmail.com>
2024-12-10 18:08:21 -0700

    2024 Day 6

---
+++ 2024/python/day6.py
@@ -0,0 +1,121 @@
+from solution import Solution
+
+FACINGS = {
+    0: (0, -1),
+    1: (1, 0),
+    2: (0, 1),
+    3: (-1, 0),
+}
+
+
+class Day6(Solution):
+    w = 0
+    h = 0
+    walls = set()
+
+    @classmethod
+    def __WalkGrid(
+        cls,
+        guardPos: tuple[int, int],
+        facing: int,
+    ) -> bool:
+        steps = set()
+        while True:
+            exited, newPos, facing = cls.__Step(guardPos, facing)
+            if exited:
+                return False
+
+            step = (facing, guardPos, newPos)
+            if step in steps:
+                return True
+
+            steps.add(step)
+            guardPos = newPos
+
+    @classmethod
+    def __Step(
+        cls,
+        guardPos: tuple[int, int],
+        facing: int,
+    ) -> tuple[bool, list[list[str]], tuple[int, int], int]:
+        dirs = FACINGS[facing]
+        nextPos = (
+            guardPos[0] + dirs[0],
+            guardPos[1] + dirs[1],
+        )
+
+        if not (0 <= nextPos[0] < cls.w and 0 <= nextPos[1] < cls.h):
+            # Exited grid
+            return True, guardPos, facing
+
+        # Rotate
+        if nextPos in cls.walls:
+            return False, guardPos, (facing + 1) % 4
+
+        # Still moving in the grid.
+        return False, nextPos, facing
+
+    @classmethod
+    def _Part1(cls):
+        guardIndex = cls.inputText.replace("\n", "").index("^")
+        cls.w, cls.h = len(cls.inputLines[0]), len(cls.inputLines)
+        guardPos = (guardIndex % cls.w, guardIndex // cls.w)
+        cls.walls = {
+            (x, y)
+            for y, line in enumerate(cls.inputLines)
+            for x, c in enumerate(line)
+            if c == "#"
+        }
+
+        visited = {guardPos}
+        facing = 0
+        while True:
+            exited, guardPos, facing = cls.__Step(guardPos, facing)
+            if exited:
+                break
+            visited.add(guardPos)
+
+        return len(visited)
+
+    @classmethod
+    def _Part2(cls):
+        guardIndex = cls.inputText.replace("\n", "").index("^")
+        cls.w, cls.h = len(cls.inputLines[0]), len(cls.inputLines)
+        guardPos = (guardIndex % cls.w, guardIndex // cls.w)
+        cls.walls = {
+            (x, y)
+            for y, line in enumerate(cls.inputLines)
+            for x, c in enumerate(line)
+            if c == "#"
+        }
+
+        facing = 0
+        options = 0
+        visited = {guardPos}
+        while True:
+            exited, guardPos, facing = cls.__Step(guardPos, facing)
+            visited.add(guardPos)
+
+            dirs = FACINGS[facing]
+            nextPos = (
+                guardPos[0] + dirs[0],
+                guardPos[1] + dirs[1],
+            )
+
+            if (
+                nextPos not in visited
+                and nextPos not in cls.walls
+                and (0 <= nextPos[0] < cls.w and 0 <= nextPos[1] < cls.h)
+            ):
+                cls.walls.add(nextPos)
+                if cls.__WalkGrid(guardPos, facing):
+                    options += 1
+                cls.walls.remove(nextPos)
+
+            if exited:
+                break
+
+        return options
+
+
+Day6.Run("day6.txt")
