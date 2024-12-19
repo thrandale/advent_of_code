7148e691d59587734f740646442ac4864bc2dcf9
Theo Crandall <theo.crandall@computronix.com>
2024-12-19 11:45:59 -0700

    2024 Day 19

---
+++ 2024/python/day19.py
@@ -0,0 +1,36 @@
+import re
+
+from functools import cache
+from solution import Solution
+
+
+class Day19(Solution):
+    towels = None
+
+    @staticmethod
+    @cache
+    def CountWays(design):
+        return (
+            sum(
+                Day19.CountWays(design[len(towel) :])
+                for towel in Day19.towels
+                if design.startswith(towel)
+            )
+            if design
+            else 1
+        )
+
+    @classmethod
+    def Part1(cls) -> int:
+        towelsRegex = cls.inputLines[0].replace(", ", "|")
+        regex = re.compile(rf"(^|(?<=\n))({towelsRegex})+($|\n)")
+        return len(regex.findall(cls.inputBlocks[1]))
+
+    @classmethod
+    def Part2(cls) -> int:
+        cls.towels = cls.inputLines[0].split(", ")
+        designs = cls.inputBlocks[1].splitlines()
+        return sum(cls.CountWays(design) for design in designs)
+
+
+Day19.Run("day19.txt")
