ab38907a1946fda1922949f3c314e1e009d812d4
Theo Crandall <thrandale@gmail.com>
2024-12-30 21:23:57 -0700

    2024 Day 25

---
+++ 2024/python/day25.py
@@ -0,0 +1,25 @@
+from solution import Solution
+
+
+class Day25(Solution):
+    @classmethod
+    def Part1(cls):
+        locks, keys = [], []
+        for schematic in cls.inputBlocks:
+            columns = [line.count("#") for line in zip(*schematic.splitlines())]
+            if schematic.startswith("#####"):
+                keys.append(columns)
+            else:
+                locks.append(columns)
+
+        return sum(
+            all(k + l <= 7 for k, l in zip(key, lock)) for key in keys for lock in locks
+        )
+
+    @classmethod
+    def Part2(cls):
+        # There is no part 2
+        return 0
+
+
+Day25.Run("day25.txt")
